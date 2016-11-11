ddd89156de84f11c01c7d5cdb514d68b7e6c9dfc
Jenny Kats <y.jennykats@gmail.com>
2016-11-11 22:43:24 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime? (n)
+prime = false
+    if n == 2 || n == 3
+      prime = true
+    elsif (n % 2 != 0) && (n % 3 != 0) && (n > 1)
+      prime = true
+    end
+  return prime
+end
